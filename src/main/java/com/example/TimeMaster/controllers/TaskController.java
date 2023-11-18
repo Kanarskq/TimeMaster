2c58611776814cb9eb859de469fa9a75f083f3f7
Kanarskq <anatoliykanarskiy075@gmail.com>
2023-11-18 22:40:47 +0100

    Editing information about a task

--- src/main/java/com/example/TimeMaster/controllers/TaskController.java
+++ src/main/java/com/example/TimeMaster/controllers/TaskController.java
@@ -6,10 +6,7 @@ import com.example.TimeMaster.repositories.TaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -54,4 +51,35 @@ public class TaskController {
         model.addAttribute("task", task);
         return "task";
     }
+
+    @GetMapping("/usersTasks/task/{id}/edit")
+    public String editTaskForm(@PathVariable("id") Long id, Model model) {
+        Task task = taskRepository.findById(id).orElse(null);
+
+        if (task != null) {
+            model.addAttribute("task", task);
+            return "editTask";
+        } else {
+            // Handle the case where the task is not found
+            return "redirect:/usersTasks"; // Redirect to the task list
+        }
+    }
+
+    @PostMapping("/usersTasks/task/{id}/edit")
+    public String editTaskSubmit(@PathVariable("id") Long id, @ModelAttribute Task updatedTask) {
+        Task existingTask = taskRepository.findById(id).orElse(null);
+
+        if (existingTask != null) {
+            // Update the existing task with new details
+            existingTask.setTaskName(updatedTask.getTaskName());
+            existingTask.setDescription(updatedTask.getDescription());
+            existingTask.setDueDate(updatedTask.getDueDate());
+            // Update other fields as needed
+
+            taskRepository.save(existingTask);
+        }
+
+        return "redirect:/usersTasks/task/{id}";
+    }
+
 }
